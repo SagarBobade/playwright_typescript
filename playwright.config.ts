945a4fcc590bdbb77bfb1b34c6bf75ec17a4fe8b
Sagar Bobade <sagarbobade@Sagars-MacBook-Pro.local>
2026-01-14 14:59:25 +0530

    test: added github workflow file

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   reporter: [
     ["list"], // console summary
     // ['allure-playwright', {
@@ -23,9 +23,9 @@ export default defineConfig({
   ],
   use: {
     baseURL: 'https://rahulshettyacademy.com/',
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     channel: 'chrome',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     viewport: { width: 1280, height: 720 }, 
